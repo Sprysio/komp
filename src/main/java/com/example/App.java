015faec448490d4d625baba3961ae5c233f019b1
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-10-17 08:37:03 +0200

    another one

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World2!" );
         System.out.println("fwaf");
+        System.out.println("inapropiate");
     }
 }
